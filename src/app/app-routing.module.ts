53a9113c4a29d6c1ef0fb34113b802e3e9e7b241
Kostas Tsangopoulos <ktsangop@gmail.com>
2020-06-18 16:19:28 +0300

    init

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,32 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { IntroComponent } from './intro/intro.component';
+import { MainComponent } from './main/main.component';
+import { HeaderOneComponent } from './header-one/header-one.component';
+import { HeaderTwoComponent } from './header-two/header-two.component';
 
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: '', pathMatch: 'full', redirectTo: 'intro'},
+  {
+    path: 'intro',
+    component: IntroComponent
+  },
+  {
+    path: 'intro',
+    component: HeaderOneComponent,
+    outlet: 'header'
+  },
+  {
+    path: 'main',
+    component: MainComponent
+  },
+  {
+    path: 'main',
+    component: HeaderTwoComponent,
+    outlet: 'header'
+  },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,18 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderOneComponent } from './header-one/header-one.component';
+import { HeaderTwoComponent } from './header-two/header-two.component';
+import { IntroComponent } from './intro/intro.component';
+import { MainComponent } from './main/main.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderOneComponent,
+    HeaderTwoComponent,
+    IntroComponent,
+    MainComponent
   ],
   imports: [
     BrowserModule,
